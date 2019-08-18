2fb7953975eba313c684af38d0674d6c91181fd9
nobrayner <braydon@raynerhall.co>
2019-08-19 07:41:46 +1000

    Fixing mobile UI

--- src/components/TimeEntry.vue
+++ src/components/TimeEntry.vue
@@ -1,15 +1,15 @@
 <template>
-  <fieldset class="time-entry">
-    <button class="remove-time-entry" @click="removeTimeEntry">X</button>
-    <input type="text" :name="'time_entry_start_' + timeEntry.id" maxlength="8" required
+  <section class="time-entry">
+    <button :ref="'time_entry_rm_' + timeEntry.id" class="remove-time-entry" @click="removeTimeEntry">X</button>
+    <input type="text" :name="'time_entry_start_' + timeEntry.id" maxlength="8" class="time-start" required
            :value="timeEntry.timeStart" @input="timeEntryUpdate('timeStart', $event)" />
-    <input type="text" :name="'time_entry_end_' + timeEntry.id" maxlength="8" class="gray"
+    <input type="text" :name="'time_entry_end_' + timeEntry.id" maxlength="8" class="gray time-end"
            :value="timeEntry.timeEnd" @input="timeEntryUpdate('timeEnd', $event)" />
     <input :id="'time_entry_ticket_id_' + timeEntry.id" type="text" :name="'time_entry_ticket_id_' + timeEntry.id"
-           :value="timeEntry.ticketID" @input="timeEntryUpdate('ticketID', $event)" class="uppercase" required />
-    <textarea :ref="'time_entry_details_' + timeEntry.id" :name="'time_entry_details_' + timeEntry.id"
+           :value="timeEntry.ticketID" @input="timeEntryUpdate('ticketID', $event)" class="uppercase ticket-id" required />
+    <textarea :ref="'time_entry_details_' + timeEntry.id" :name="'time_entry_details_' + timeEntry.id" class="details"
               :value="timeEntry.details" @input="expand_textarea(timeEntry.id); timeEntryUpdate('details', $event)" required ></textarea>
-  </fieldset>
+  </section>
 </template>
 
 <script>
@@ -49,23 +49,4 @@ export default {
 </script>
 
 <style>
-.time-entry {
-  display: grid;
-  grid-template-columns: 2em 5.5em 6.5em 10em 1fr;
-}
-button.remove-time-entry {
-  margin: 0;
-  width: 2em;
-  background-color: red;
-  color: white;
-  border: 1px solid red;
-  opacity: 0;
-  transition: opacity 0.2s ease-in-out;
-}
-fieldset.time-entry:hover > button.remove-time-entry, fieldset.time-entry:focus > button.remove-time-entry {
-  opacity: 1;
-}
-fieldset.time-entry > *:nth-child(n+3) {
-  margin-left: 1em;
-}
 </style>
